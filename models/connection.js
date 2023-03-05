4416b0bb5c43741bd07ade99a140a84975be0dde
Christian Reubrez <christian.reubrez@gmail.com>
2023-03-05 22:11:43 +0100

    env configuration

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:fC307pzA2aZY4rHZ@cluster0.srnjetf.mongodb.net/weatherapp';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
